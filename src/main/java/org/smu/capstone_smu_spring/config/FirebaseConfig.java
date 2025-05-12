0f5457c899b6e32bee06b2da2240051a1d68fd3a
Gun-Woo-Park <pgw031203@naver.com>
2025-05-12 10:58:58 +0900

    aa

--- src/main/java/org/smu/capstone_smu_spring/config/FirebaseConfig.java
+++ src/main/java/org/smu/capstone_smu_spring/config/FirebaseConfig.java
@@ -19,11 +19,10 @@ public class FirebaseConfig {
     @PostConstruct
     public void initFirebase() {
         try {
-            String bucket = System.getenv("my-capstone-keys");       // e.g., "my-capstone-keys"
-            String key = System.getenv("serviceAccountKey.json");             // e.g., "serviceAccountKey.json"
-            String region = System.getenv("ap-southeast-2");           // e.g., "ap-southeast-2"
+            String bucket = System.getenv("S3_BUCKET_NAME");              // 예: "my-capstone-keys"
+            String key = System.getenv("FIREBASE_KEY_OBJECT_NAME");      // 예: "serviceAccountKey.json"
+            String region = System.getenv("AWS_REGION");                 // 예: "ap-southeast-2"
 
-            // 1. S3에서 serviceAccountKey.json 다운로드
             S3Client s3 = S3Client.builder()
                     .region(Region.of(region))
                     .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
@@ -37,10 +36,9 @@ public class FirebaseConfig {
             ResponseBytes<?> objectBytes = s3.getObjectAsBytes(request);
             ByteArrayInputStream credentialsStream = new ByteArrayInputStream(objectBytes.asByteArray());
 
-            // 2. Firebase App 초기화 (Firestore용)
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(credentialsStream))
-                    .build();  // Firestore는 Database URL 필요 없음
+                    .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
